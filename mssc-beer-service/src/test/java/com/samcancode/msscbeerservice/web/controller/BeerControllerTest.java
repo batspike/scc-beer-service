76e4860f11f9d5dede6b8b5fcb5a533a244819ce
Sam Lau <samlau.sam@gmail.com>
2020-07-14 16:24:56 +0800

    updated to include BeerService dependency

--- mssc-beer-service/src/test/java/com/samcancode/msscbeerservice/web/controller/BeerControllerTest.java
+++ mssc-beer-service/src/test/java/com/samcancode/msscbeerservice/web/controller/BeerControllerTest.java
@@ -10,14 +10,18 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samcancode.msscbeerservice.web.model.BeerDto;
+import com.samcancode.msscbeerservice.web.model.BeerStyleEnum;
 
-@WebMvcTest(BeerController.class)
+//@WebMvcTest(BeerController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class BeerControllerTest {
 	@Autowired
 	MockMvc mockMvc;
@@ -33,7 +37,10 @@ class BeerControllerTest {
 
 	@Test
 	void testSaveNewBeer() throws Exception {
-		BeerDto beerDto = BeerDto.builder().build();
+		BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID())
+				.beerName("Tiger Beer")
+				.beerStyle(BeerStyleEnum.PALE_ALE)
+				.build();
 		String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 		
 		mockMvc.perform(post("/api/v1/beer/").contentType(MediaType.APPLICATION_JSON).content(beerDtoJson))
