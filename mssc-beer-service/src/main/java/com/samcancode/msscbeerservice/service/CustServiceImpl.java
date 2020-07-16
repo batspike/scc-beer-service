0be1fd1e51cdc99f29697a93e317c6620a430aa6
Sam Lau <samlau.sam@gmail.com>
2020-07-16 17:12:35 +0800

    added CustomerRepository, updated CustServiceImpl

--- mssc-beer-service/src/main/java/com/samcancode/msscbeerservice/service/CustServiceImpl.java
+++ mssc-beer-service/src/main/java/com/samcancode/msscbeerservice/service/CustServiceImpl.java
@@ -1,9 +1,12 @@
 package com.samcancode.msscbeerservice.service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.samcancode.msscbeerservice.domain.Customer;
+import com.samcancode.msscbeerservice.repository.CustomerRepository;
 import com.samcancode.msscbeerservice.web.model.CustomerDto;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,22 +14,36 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class CustServiceImpl implements CustService {
+	private final CustomerRepository custRepo;
+	
+	public CustServiceImpl(CustomerRepository custRepo) {
+		this.custRepo = custRepo;
+	}
 
 	@Override
 	public CustomerDto getCustById(UUID custId) {
-		return CustomerDto.builder()
-							.id(custId)
-							.name("Paul Walker")
-							.build();
+		Optional<Customer> cust = custRepo.findById(custId);
+		
+		if (cust.isPresent()) {
+			return CustomerDto.builder()
+								.id(custId)
+								.name(cust.get().getName())
+								.build();
+		}
+		
+		return null;
 	}
 
 	@Override
 	public CustomerDto save(CustomerDto custDto) {
-		log.info("Saving customer id: "+ custDto.getId());
+		Customer cust = Customer.builder()
+								.name(custDto.getName())
+								.build();
+		Customer savedCust = custRepo.save(cust);
 		
 		return CustomerDto.builder()
-				.id(UUID.randomUUID())
-				.name(custDto.getName())
+				.id(savedCust.getId())
+				.name(savedCust.getName())
 				.build();
 	}
 
